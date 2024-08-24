6d79fb506199fd318cbed8a8fa281fe547cdb78f
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-24 17:14:51 -0400

    cleaned up logger

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { logger, validateUserId, validateUser, validatePost } = require('../middleware/middleware');
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
 const Users = require('./users-model')
 const Posts = require('../posts/posts-model')
 
@@ -22,33 +22,33 @@ router.get('/',  async (req, res) => {
   }
 });
 
-router.get('/:id', logger, (req, res) => {
+router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.post('/', logger, (req, res) => {
+router.post('/', (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', logger, (req, res) => {
+router.put('/:id', (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', logger, (req, res) => {
+router.delete('/:id', (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', logger, (req, res) => {
+router.get('/:id/posts', (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', logger, (req, res) => {
+router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
