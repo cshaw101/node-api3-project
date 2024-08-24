d511fb22c10f42ee00d844a57aee46c062049f84
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-24 13:01:15 -0400

    server is up and running

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
